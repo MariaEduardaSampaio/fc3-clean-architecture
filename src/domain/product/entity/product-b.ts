2d9b5f583910378d02e8acdae61a167cbdd4a1bc
Maria Eduarda Sampaio <maria.sampaio@me.com.br>
2025-12-25 17:22:26 -0300

    refactor: Refactor product validation by introducing ProductValidatorFactory and ProductYupValidator

--- src/domain/product/entity/product-b.ts
+++ src/domain/product/entity/product-b.ts
@@ -1,64 +1,47 @@
 import Entity from "../../@shared/entity/entity.abstract";
 import NotificationError from "../../@shared/notification/notification.error";
+import ProductValidatorFactory from "../factory/product.validator.factory";
 import ProductInterface from "./product.interface";
 
 export default class ProductB extends Entity implements ProductInterface {
-  private _name: string;
-  private _price: number;
+	private _name: string;
+	private _price: number;
 
-  constructor(id: string, name: string, price: number) {
-    super();
-    this._id = id;
-    this._name = name;
-    this._price = price;
-    this.validate();
+	constructor(id: string, name: string, price: number) {
+		super();
+		this._id = id;
+		this._name = name;
+		this._price = price;
+		this.validate();
 
-    if (this.notification.hasErrors()) {
-      throw new NotificationError(this.notification.getErrors());
-    }
-  }
+		if (this.notification.hasErrors()) {
+			throw new NotificationError(this.notification.getErrors());
+		}
+	}
 
-  get id(): string {
-    return this._id;
-  }
+	get id(): string {
+		return this._id;
+	}
 
-  get name(): string {
-    return this._name;
-  }
+	get name(): string {
+		return this._name;
+	}
 
-  get price(): number {
-    return this._price * 2;
-  }
+	get price(): number {
+		return this._price * 2;
+	}
 
-  changeName(name: string): void {
-    this._name = name;
-    this.validate();
-  }
+	changeName(name: string): void {
+		this._name = name;
+		this.validate();
+	}
 
-  changePrice(price: number): void {
-    this._price = price;
-    this.validate();
-  }
+	changePrice(price: number): void {
+		this._price = price;
+		this.validate();
+	}
 
-  validate(): boolean {
-    if (this._id.length === 0) {
-      this.notification.addError({
-        message: "Id is required",
-        context: "product"
-      });
-    }
-    if (this._name.length === 0) {
-      this.notification.addError({
-        message: "Name is required",
-        context: "product"
-      });
-    }
-    if (this._price < 0) {
-      this.notification.addError({
-        message: "Price must be greater than zero",
-        context: "product"
-      });
-    }
-    return true;
-  }
+	validate() {
+		ProductValidatorFactory.create().validate(this);
+	}
 }
